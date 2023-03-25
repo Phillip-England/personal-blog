13b80560baf9d012ce4580ea2394acfdf0f3503c
Phillip-England <Phillip.mark.england@gmail.com>
2023-03-25 00:38:45 -0500

    testing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
         black: "#201c1c",
         white: "#e3e3e3",
         blue: "#0852c9",
-        main: "#048004",
+        main: "#201c1c",
+        lightgray: "#c9c9c9",
       },
     },
   },
