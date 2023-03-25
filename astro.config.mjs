4b9d0103ae28795c98583b26d21910cd13269403
Phillip-England <Phillip.mark.england@gmail.com>
2023-03-25 00:52:24 -0500

    added nelify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
+
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://phillip-england.github.io",
   base: "/personal-blog",
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
 })
